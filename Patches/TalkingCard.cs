ee3c6184885a475cc9875967afa2d6bf6b80679c
RykeDaxter <RykeDaxter@gmail.com>
2022-06-20 21:40:48 +0800

    Add project files.

---
+++ Patches/TalkingCard.cs
@@ -0,0 +1,25 @@
+﻿using DiskCardGame;
+using HarmonyLib;
+using System.Linq;
+
+namespace KCTalkingCards
+{
+	[HarmonyPatch(typeof(TalkingCard), "OnDrawn")]
+	class AscensionTalkingCardPatch
+	{
+		[HarmonyPrefix]
+		static bool AscensionOnDrawn(TalkingCard __instance)
+		{
+			if (SaveFile.IsAscension)
+			{
+				string[] talkingCardsToSkipOnDrawn = { "DiskCardGame.StoatTalkingCard", "DiskCardGame.StinkbugTalkingCard", "DiskCardGame.WolfTalkingCard" };
+
+				if (talkingCardsToSkipOnDrawn.Contains(__instance.GetType().ToString()))
+				{
+					return false;
+				}
+			}
+			return true;
+		}
+	}
+}
